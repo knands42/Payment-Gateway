2d22ebe27408005c7f0516aebcaf69854fa0106b
Caio Fernandes <caiow.wk@gmail.com>
2023-02-14 20:19:19 -0300

    feat: add transaction entity

--- domain/entity/transaction.go
+++ domain/entity/transaction.go
@@ -1,1 +1,58 @@
 package entity
+
+import "errors"
+
+type Transaction struct {
+	ID           string
+	AccountId    string
+	Amount       float64
+	CreditCard   CreditCard
+	Status       string
+	ErrorMessage string
+}
+
+func NewTransaction(id, accountId string, amount float64) (*Transaction, error) {
+	t := &Transaction{
+		ID:        id,
+		AccountId: accountId,
+		Amount:    amount,
+	}
+
+	if err := t.IsValid(); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (t *Transaction) IsValid() error {
+	if !t.validateID() {
+		return errors.New("invalid transaction id")
+	}
+
+	if err := t.validateAmount(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Transaction) SetCreditCard(cc CreditCard) {
+	t.CreditCard = cc
+}
+
+func (t *Transaction) validateID() bool {
+	return t.ID != ""
+}
+
+func (t *Transaction) validateAmount() error {
+	if t.Amount < 1 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if t.Amount > 1000 {
+		return errors.New("limit exceeded")
+	}
+
+	return nil
+}
--- domain/entity/transaction_test.go
+++ domain/entity/transaction_test.go
@@ -1,1 +1,37 @@
 package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_TransactionIdValidation(t *testing.T) {
+	// valid
+	_, err := NewTransaction("123", "123", 100)
+	assert.Nil(t, err)
+
+	// invalid
+	_, err = NewTransaction("", "123", 100)
+	assert.Equal(t, "invalid transaction id", err.Error())
+}
+
+func Test_TrasactionAmountValidation(t *testing.T) {
+	// valid
+	_, err := NewTransaction("123", "123", 100)
+	assert.Nil(t, err)
+
+	// invalid
+	_, err = NewTransaction("123", "123", 0)
+	assert.Equal(t, "the amount must be greater than 0", err.Error())
+
+	_, err = NewTransaction("123", "123", 1001)
+	assert.Equal(t, "limit exceeded", err.Error())
+}
+
+func Test_SetCreditCard(t *testing.T) {
+	cc, _ := NewCreditCard("4111111111111111", "John Doe", 1, 2021, "123")
+	transaction, _ := NewTransaction("123", "123", 100)
+	transaction.SetCreditCard(*cc)
+	assert.Equal(t, cc, &transaction.CreditCard)
+}
